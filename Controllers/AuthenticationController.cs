b2a7833a31eb200bd00f7afa9d6794a59b46b59f
Abisagy Nafula <abisakyn@gmail.com>
2024-05-19 10:20:03 +0300

    added the confirmation of eail functionality

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -17,64 +17,68 @@ namespace LearningApp.Controllers
         private readonly IConfiguration _configuration;
         private readonly IEmailService _emailService;
 
-
-        public AuthenticationController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration, IEmailService emailService)
+        public AuthenticationController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration, IEmailService emailService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _roleManager = roleManager;
-
             _configuration = configuration;
             _emailService = emailService;
-
         }
 
         [HttpPost]
-
-        public async Task<IActionResult> Register(Register register , string role)
+        public async Task<IActionResult> Register(Register register, string role)
         {
-            if(ModelState.IsValid ==false)
+            if (ModelState.IsValid == false)
             {
-                string errrorMessage = string.Join("", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
-                return Problem(errrorMessage);
+                string errorMessage = string.Join("", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+                return Problem(errorMessage);
             }
-            //check if user exists in the database
-             var userExists  = await _userManager.FindByEmailAsync(register.Email!);
-             if(userExists != null)
-            { 
-                return StatusCode (StatusCodes.Status403Forbidden, new Response
+
+            // Check if user exists in the database
+            var userExists = await _userManager.FindByEmailAsync(register.Email!);
+            if (userExists != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, new Response
                 {
-                    Status ="Error",Message = "User already Exists!!"
+                    Status = "Error",
+                    Message = "User already exists!"
                 });
             }
 
-            //create user in the database
+            // Create user in the database
             IdentityUser user = new()
             {
                 Email = register.Email,
                 UserName = register.Username
             };
 
-            if(await _roleManager.RoleExistsAsync(role))
+            if (await _roleManager.RoleExistsAsync(role))
             {
-                var results = await _userManager.CreateAsync(user, register.Password!);
+                var result = await _userManager.CreateAsync(user, register.Password!);
 
-                if (!results.Succeeded)
+                if (!result.Succeeded)
                 {
                     return StatusCode(StatusCodes.Status500InternalServerError, new Response
                     {
                         Status = "Error",
-                        Message = "User Failed to create"
+                        Message = "User failed to create"
                     });
                 }
 
-                //add role to the user.....
-               await _userManager.AddToRoleAsync(user, role);
+                // Add role to the user
+                await _userManager.AddToRoleAsync(user, role);
+
+                // Add token to verify the email
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authentication", new { token, email = user.Email }, Request.Scheme);
+                var message = new Message(new string[] { user.Email! }, "Confirmation Email Link", confirmationLink!);
+                _emailService.SendEmail(message);
 
                 return StatusCode(StatusCodes.Status201Created, new Response
                 {
                     Status = "Success",
-                    Message = "User Created Successfully"
+                    Message = $"User created successfully and email sent to {user.Email} successfully"
                 });
             }
             else
@@ -85,22 +89,23 @@ namespace LearningApp.Controllers
                     Message = "This role does not exist"
                 });
             }
-
-            //Asign role
-
         }
 
-        [HttpGet]
-
-        public IActionResult TestEmail()
+        [HttpGet("ConfirmEmail")]
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
         {
-            var message = new Message(new string[] { "abisakyn@gmail.com" }, "Test", "Subscribe to my youtube channel");
-            return StatusCode(StatusCodes.Status200OK, new Response
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user != null)
             {
-                Status = "Success",
-                Message = "Email sent successfully"
-            });
-
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+                if (result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status200OK,
+                        new Response { Status = "Success", Message = "Email verified successfully" });
+                }
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new Response { Status = "Error", Message = "This user does not exist" });
         }
     }
 }
