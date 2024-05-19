8041882cf47d7ee477b2b23f26bd9e9c2e07c7f9
Abisagy Nafula <abisakyn@gmail.com>
2024-05-19 16:22:54 +0300

    Added admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LearningApp.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "Abbie", "Nafula", "Wanyonyi" };
+        }
+    }
+}
